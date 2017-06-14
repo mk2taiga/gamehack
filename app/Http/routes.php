15728443378f11938d31a3bb03f22c4a34da0e89
Taiga Miyazaki <mk2_tg000@icloud.com>
2017-06-14 06:19:40 +0000

    register

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', 'WelcomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ユーザ登録用のルーティング
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-
+avbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">GameHack</a>
+                <form class="navbar-form navbar-left" role="search">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                  </div>
+                  <button type="submit" class="btn btn-default">検索</button>
+                </form>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('signup.get') }}">新規登録</a></li>
+                    <li><a href="#">ログイン</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+
